676280f9fd59abe0592bc382f223361104e3b1c6
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2020-04-09 20:33:58 -0300

    Created a login scene

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -1,12 +1,39 @@
 package org.example;
 
-import java.io.IOException;
+
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    public PasswordField passwordField;
+
+    @FXML
+    public TextField txtFieldUsuario;
+
+    @FXML
+    public Button login;
+
+    @FXML
+    public Label warning;
+
+    @FXML
+    public void switchToMain(ActionEvent event) throws IOException {
+        String user = "iago";
+        String pass = "12345";
+        if(txtFieldUsuario.getText().equals(user) && passwordField.getText().equals(pass)){
+            App.setRoot("secondary");
+        } else {
+            warning.setText("Usuário ou senha incorreto.");
+        }
     }
+
 }
