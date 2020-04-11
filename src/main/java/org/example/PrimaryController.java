a2b44b387f3c4c2e55967c458faf277bca9ae91f
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2020-04-11 00:41:15 -0300

    ###Improvements
    in the textField on the login screen - added event

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -1,17 +1,21 @@
 package org.example;
 
 
-
-import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable {
 
     @FXML
     public PasswordField passwordField;
@@ -26,14 +30,50 @@ public class PrimaryController {
     public Label warning;
 
     @FXML
-    public void switchToMain(ActionEvent event) throws IOException {
+    public void switchToMain() {
         String user = "iago";
         String pass = "12345";
-        if(txtFieldUsuario.getText().equals(user) && passwordField.getText().equals(pass)){
-            App.setRoot("secondary");
+        if (txtFieldUsuario.getText().equals(user) && passwordField.getText().equals(pass)) {
+            try {
+                App.setRoot("secondary");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } else {
             warning.setText("Usuário ou senha incorreto.");
         }
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setTextFields();
+    }
+
+    public void setTextFields() {
+        passwordField.setFocusTraversable(false);
+        txtFieldUsuario.requestFocus();
+
+        txtFieldUsuario.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.ENTER) {
+                    passwordField.requestFocus();
+                }
+            }
+        });
+
+        passwordField.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.ENTER) {
+                    if (txtFieldUsuario.getText().equals("")) {
+                        txtFieldUsuario.requestFocus();
+                    } else {
+                        switchToMain();
+                    }
+                }
+            }
+        });
+    }
 }
+
