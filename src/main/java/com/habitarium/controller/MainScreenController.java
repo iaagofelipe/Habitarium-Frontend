9d54be7813225bb3eb3961218408adea5a5ff9d9
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2020-04-17 19:31:19 -0300

    ##Created
    Created debtor table columns
    Created its attributes in the control class

--- src/main/java/com/habitarium/controller/MainScreenController.java
+++ src/main/java/com/habitarium/controller/MainScreenController.java
@@ -3,6 +3,9 @@ package com.habitarium.controller;
 import com.habitarium.utils.ScreenUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import main.java.entity.Lessor;
 
 import java.io.IOException;
 
@@ -10,18 +13,20 @@ public class MainScreenController {
 
     @FXML
     private Button registerPropertyBtn;
-
     @FXML
     private Button registerRentBtn;
-
     @FXML
     private Button searchRentBtn;
-
     @FXML
     private Button searchLessorBtn;
-
     @FXML
     private Button searcPropertyBtn;
+    @FXML
+    private TableView<Lessor> tabViewDebtors;
+    @FXML
+    private TableColumn<Lessor, String> tabColName;
+    @FXML
+    private TableColumn<Lessor, String> tabColDate;
 
     @FXML
     public void registerProperty() {
