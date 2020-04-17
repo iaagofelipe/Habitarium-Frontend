eeaf7be7d1ad780a7478bc13cf907f5f2d002992
LaBatata101 <labatata101@linuxmail.org>
2020-04-17 15:58:11 -0300

    throw IOException when file is not found

--- src/main/java/com/habitarium/utils/ScreenUtils.java
+++ src/main/java/com/habitarium/utils/ScreenUtils.java
@@ -1,19 +1,22 @@
-package com.habitarium;
+package com.habitarium.utils;
 
-import com.habitarium.controller.MainScreenController;
+import com.habitarium.App;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class ScreenUtils {
     public static void switchScreen(String screen, String title) throws IOException {
-        Parent root;
-        FXMLLoader fxmlLoader = new FXMLLoader(MainScreenController.class.getResource(
-                screen));
-        root = fxmlLoader.load();
+        URL url = App.class.getResource(screen + ".fxml");
+        if (url == null) {
+            throw new IOException("File \"" + screen + ".fxml\" doesn't exists.");
+        }
+        FXMLLoader fxmlLoader = new FXMLLoader(url);
+        Parent root = fxmlLoader.load();
         Stage stage = new Stage();
         stage.setTitle(title);
         stage.setScene(new Scene(root));
