6cc37d63045a427eb4f8cb85a2009ade3139d07a
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2020-04-21 00:31:47 -0300

    Fx id and created atributes

--- src/main/java/com/habitarium/controller/RegisterPropertyScreenController.java
+++ src/main/java/com/habitarium/controller/RegisterPropertyScreenController.java
@@ -1,4 +1,33 @@
 package com.habitarium.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class RegisterPropertyScreenController {
+
+    @FXML
+    private ChoiceBox<?> chooseTypeProperty;
+    @FXML
+    private TextField txtStreet;
+    @FXML
+    private TextField txtNumber;
+    @FXML
+    private TextField txtNeighbourneighbour;
+    @FXML
+    private TextField txtCity;
+    @FXML
+    private TextField txtCondo;
+    @FXML
+    private TextField txtApartmentapartment;
+    @FXML
+    private TextField txtBlockCondoblockCondo;
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    void save() {
+        
+    }
 }
