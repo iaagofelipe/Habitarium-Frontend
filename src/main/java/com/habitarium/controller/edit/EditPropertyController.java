8c034adc2cac7d212fcc1f3911913ed85cc0f1a0
Iago Felipe <iaagofelipe@gmail.com>
2020-05-15 14:34:26 -0300

    added trash button with icons an edit controller

--- src/main/java/com/habitarium/controller/edit/EditPropertyController.java
+++ src/main/java/com/habitarium/controller/edit/EditPropertyController.java
@@ -1,15 +1,56 @@
 package com.habitarium.controller.edit;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import main.java.entity.Property;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class EditPropertyController {
+
+public class EditPropertyController implements Initializable {
+//    @FXML
+//    private Label txtLabel;
+
+@FXML
+    private TextField tfStreet;
+
     @FXML
-    private Label txtLabel;
+    private TextField tfNumber;
 
-    public void initializeScreen(Property property) {
-        txtLabel.setText(property.getStreet());
+    @FXML
+    private TextField tfNeighbour;
+
+    @FXML
+    private TextField tfCity;
+
+    @FXML
+    private TextField tfCondo;
+
+    @FXML
+    private TextField tfApartment;
+
+    @FXML
+    private TextField tfBlockCondo;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnDelete;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
+
+//    public void initializeScreen(Property property) {
+//        txtLabel.setText(property.getStreet());
+//    }
+
+
 }
