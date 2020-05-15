e96d6bed99a1ffec265999872a808bb44313bd35
Iago Felipe <iaagofelipe@gmail.com>
2020-05-15 15:55:15 -0300

    added save method and check padding

--- src/main/java/com/habitarium/controller/edit/EditPropertyController.java
+++ src/main/java/com/habitarium/controller/edit/EditPropertyController.java
@@ -2,9 +2,9 @@ package com.habitarium.controller.edit;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import main.java.dao.PropertyDAO;
 import main.java.entity.Property;
 
 import java.net.URL;
@@ -57,5 +57,60 @@ public class EditPropertyController implements Initializable {
         tfBlockCondo.setText(property.getBlockCondo());
     }
 
+    @FXML
+    void save() {
+        if (checkPadding()) {
+            Property property = new Property();
+            property.setBlockCondo(tfBlockCondo.getText().trim());
+            property.setCity(tfCity.getText().trim());
+            property.setCondo(tfCondo.getText().trim());
+            property.setNeighbour(tfNeighbour.getText().trim());
+            property.setPropertyNumber(tfNumber.getText().trim());
+            property.setStreet(tfStreet.getText().trim());
+            property.setApartment(tfApartment.getText().trim());
+            PropertyDAO propertyDAO = new PropertyDAO();
+            propertyDAO.update(property);
+            saveSucess();
+            Stage stage = (Stage) btnSave.getScene().getWindow();
+            stage.close();
+        } else {
+            alertPadding();
+        }
+    }
 
+    public boolean checkPadding() {
+        boolean isApartment = !tfApartment.getText().trim().equals("") && !tfCondo.getText().trim().equals("")
+                && !tfBlockCondo.getText().trim().equals("") && !tfCity.getText().trim().equals("")
+                && !tfNeighbour.getText().trim().equals("") && !tfNumber.getText().trim().equals("")
+                && !tfStreet.getText().trim().equals("");
+        boolean isHouse = !tfCity.getText().trim().equals("")
+                && !tfNeighbour.getText().trim().equals("") && !tfNumber.getText().trim().equals("")
+                && !tfStreet.getText().trim().equals("");
+        boolean isCondo = !tfCondo.getText().trim().equals("")
+                && !tfBlockCondo.getText().trim().equals("") && !tfCity.getText().trim().equals("")
+                && !tfNeighbour.getText().trim().equals("") && !tfNumber.getText().trim().equals("")
+                && !tfStreet.getText().trim().equals("");
+
+        if (isApartment || isHouse || isCondo) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void alertPadding() {
+        Alert alert = new Alert(Alert.AlertType.ERROR, "Há campos em branco",
+                ButtonType.OK);
+        alert.setTitle("");
+        alert.setHeaderText("Erro ao preencher");
+        alert.show();
+    }
+
+    private void saveSucess() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "",
+                ButtonType.OK);
+        alert.setTitle("");
+        alert.setHeaderText("Propriedade editada com sucesso!");
+        alert.show();
+    }
 }
