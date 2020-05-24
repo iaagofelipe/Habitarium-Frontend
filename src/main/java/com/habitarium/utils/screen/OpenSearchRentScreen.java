2ffd338923902204e47bb093dc044bbcf5b04d9a
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2020-05-23 21:13:34 -0300

    Added class OpenSearchRentScreen

---
+++ src/main/java/com/habitarium/utils/screen/OpenSearchRentScreen.java
@@ -0,0 +1,37 @@
+package com.habitarium.utils.screen;
+
+import com.habitarium.App;
+import com.habitarium.controller.search.SearchPropertyScreenController;
+import com.habitarium.controller.search.SearchRentScreenController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class OpenSearchRentScreen implements OpenScreens{
+    @Override
+    public void loadScreen(String screen, String title, Object object) throws IOException {
+        FXMLLoader fxmlLoader;
+        URL url = App.class.getResource(screen + ".fxml");
+        if (url == null) {
+            throw new IOException("File \"" + screen + ".fxml\" doesn't exists.");
+        } else {
+            fxmlLoader = new FXMLLoader(url);
+            Parent root = fxmlLoader.load();
+            SearchRentScreenController searchRentScreenController = fxmlLoader.getController();
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            stage.setScene(new Scene(root));
+            stage.setResizable(false);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+            stage.focusedProperty().addListener((observable, oldNode, newNode) -> {
+
+            });
+        }
+    }
+}
