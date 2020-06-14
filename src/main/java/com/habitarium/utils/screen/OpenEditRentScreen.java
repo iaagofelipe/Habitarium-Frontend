7b5833182f0523d47079e801644732d17a6b883d
LaBatata101 <labatata101@linuxmail.org>
2020-06-14 18:43:54 -0300

    implement setReload method

--- src/main/java/com/habitarium/utils/screen/OpenEditRentScreen.java
+++ src/main/java/com/habitarium/utils/screen/OpenEditRentScreen.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.net.URL;
 
 public class OpenEditRentScreen implements OpenScreens {
+    private Reloadable reloadable;
     @Override
     public void loadScreen(String screen, String title, Object object) throws IOException {
         Rent rent = (Rent) object;
@@ -31,12 +32,13 @@ public class OpenEditRentScreen implements OpenScreens {
             stage.setScene(new Scene(root));
             stage.setResizable(false);
             stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setOnHiding(e -> reloadable.reload());
             stage.show();
         }
     }
 
     @Override
     public void setReload(Reloadable reloadable) {
-        return;
+        this.reloadable = reloadable;
     }
 }
